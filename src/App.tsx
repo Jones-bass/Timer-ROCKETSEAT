0914d81c9c66c1eb3b90b1a5d4a0bd5f6671bd0c
jones-bass <jonesbass.tb@gmail.com>
2022-07-31 18:04:59 -0300

    Estrurando Rotas e Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
+import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="primary" />
-      <Button variant="primary" />
-      <Button variant="primary" />
-      <Button />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import { Timer, Scroll } from 'phosphor-react'
+import { HeaderContainer } from './styles'
+import logoIgnite from '../../assets/logo-ignite.svg'
+import { NavLink } from 'react-router-dom'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={logoIgnite} alt="" />
+      <nav>
+        <NavLink to="/" title="Tempo">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="history" title="Historico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
