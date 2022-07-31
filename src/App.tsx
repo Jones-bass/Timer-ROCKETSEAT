e419b71b352f4574948d4def10ba76927d55a2df
jones-bass <jonesbass.tb@gmail.com>
2022-07-30 21:40:15 -0300

    Styled-Component

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from "styled-components";
+import { Button } from "./components/Button";
+import { defaultTheme } from "./styles/themes/default";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+    <Button variant='primary' />
+    <Button variant='primary' />
+    <Button variant='primary' />
+    <Button variant='primary' />
+    <Button />
+    </ThemeProvider>
+  )
+}
+
