783ca1f345ae393783180df317d7e693d297bca0
jones-bass <jonesbass.tb@gmail.com>
2022-08-01 22:42:17 -0300

    Tela Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,45 @@
+import { Play } from 'phosphor-react'
+import {
+  CountContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+  StartCountdownButton,
+  MinutesAmountInput,
+  TaskInput,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput id="task" placeholder="Dê um nomepara o seu projeto" />
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountContainer>
+
+        <StartCountdownButton type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
+}
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -15,6 +15,7 @@ export const GlobalStyle = createGlobalStyle`
 body {
   background: ${(props) => props.theme.gray900};
   color: ${(props) => props.theme.gray300};
+  -webkit-font-smoothing: antialiased;
 }
 
 body, input, textarea, button {
